a60cb0d1ccdc219a36f26970941d9617948351ac
finleypeng <weilllyox1020@gmail.com>
2022-11-15 20:32:05 +0800

    feat: 新增三頁 api 1. /api/attractions 2. /api/attraction/{attractionId} 3. /api/categories
    fix: 調整資料庫連線不穩定問題
    問題：
    1. 程式剛啓動時，api ，資料都可以正常顯示
    2. 測試一陣子 api 時，開始抓不到資料，過一陣子又可以
    
    原因：
    1. 如果是 api 查不到資料或其他非正常狀況，資料庫連線並沒有終止，資源無法釋放
    2. 當上述狀況一多時，連線池資源不夠就會無法執行指令
    3. 目前只有使用者成功查到資料時，會執行關閉資料庫
    
    調整項目：
    1. 在所有可能執行出狀況的 except, if 判斷式中加入關閉連線指令
    2. 調高連線池可連線數

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import *
+import json
+import mysql.connector
+from mysql.connector import pooling  
+from data.connect_to_db import conn, selectDb, close
+from data.error_message import errorMessage
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+
 
 # Pages
 @app.route("/")
@@ -16,5 +24,159 @@ def booking():
 @app.route("/thankyou")
 def thankyou():
 	return render_template("thankyou.html")
+
+# API
+@app.route("/api/attractions")
+def attractions():
+	page = request.args.get("page")
+	keyword = request.args.get("keyword")
+
+	if (page == None or page == ""):
+		return errorMessage("請指定頁數"), 500
+	if (keyword == ""):
+		return errorMessage("請輸入關鍵字"), 500
+
+	c = conn()
+	cursor = selectDb(c)
+	try:	
+		startAtt = int(page)*12 +1
+		sql = 'select count(id) from attraction'
+		cursor.execute(sql)
+		result = cursor.fetchone()
+		
+	except:
+		close(c, cursor)
+		return errorMessage("請輸入正確頁數"), 500
+
+
+	if (startAtt > result[0]):
+		close(c, cursor)
+		return errorMessage("已無資料可顯示"), 500
+		
+
+	i = startAtt
+	attArray = []
+	while (i < startAtt+12):
+		if (keyword == None):
+			sql = 'SELECT A.*, Cat.category FROM attraction as A INNER JOIN attraction_category as Cat ON A.att_id = Cat.att_id where A.id = %s'
+			startAttFrom = (i, )
+		
+		if (keyword != None):
+			sql = "SELECT A.*, Cat.category FROM attraction as A INNER JOIN attraction_category as Cat ON A.att_id = Cat.att_id where A.id = %s and (A.name like %s or Cat.category = %s)"
+			startAttFrom = (i, '%'+keyword+'%', keyword)
+		
+		try:
+			cursor.execute(sql, startAttFrom)
+			result = cursor.fetchone()
+		except:
+			close(c, cursor)
+			return errorMessage("關鍵字或頁數有誤"), 500
+
+		if (not result):
+			i+= 1
+			continue
+		
+		attraction = {
+			'id': result[0],
+			'name': result[2],
+			'category': result[10],
+			'description': result[8],
+			'address':result[3],
+			'direction': result[5],
+			'mrt': result[4],
+			'lat': float(result[7]),
+			'lng': float(result[6]),
+			'images': eval(result[9])
+		}
+
+		attArray.append(attraction)
+		i+= 1
+	
+	if (attArray == []):
+		close(c, cursor)
+		return errorMessage("查無資料"), 500
+	
+	try:
+		attractions = jsonify({
+			'nextpage': int(page)+1,
+			'data': attArray
+		})
+		return attractions
+
+	finally:
+		close(c, cursor)
+		print('here')
+
+@app.route("/api/attractions/<attractionId>")
+def useIdFindAttraction(attractionId):
+	try:
+		c = conn()
+		cursor = selectDb(c)
+		sql = 'SELECT A.*, Cat.category FROM attraction as A INNER JOIN attraction_category as Cat ON A.att_id = Cat.att_id where A.id = %s'
+		attId = (int(attractionId), )
+		cursor.execute(sql, attId)
+		result = cursor.fetchone()
+	except ValueError:
+		close(c, cursor)
+		return errorMessage("請輸入正確編號"), 400
+	except:
+		close(c, cursor)
+		return errorMessage("伺服器出現錯誤"), 500
+	
+	try:
+		data = {
+			'id': result[0],
+			'name': result[2],
+			'category': result[10],
+			'description': result[8],
+			'address':result[3],
+			'direction': result[5],
+			'mrt': result[4],
+			'lat': float(result[7]),
+			'lng': float(result[6]),
+			'images': eval(result[9])
+		}
+	except TypeError:
+		close(c, cursor)
+		return errorMessage("查無此編號"), 400
+	except:
+		close(c, cursor)
+		return errorMessage("伺服器出現錯誤"), 500
+
+	try:
+		attraction = jsonify({
+			'data': data
+		})
+		return attraction
+
+	finally:
+		close(c, cursor)
+
+@app.route("/api/categories")
+def categories():
+	try:
+		c = conn()
+		cursor = selectDb(c)
+		sql = 'select distinct category from attraction_category'
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		data = []
+		for i in result:
+			data.append(i[0])
+
+	except:
+		close(c, cursor)
+		return errorMessage("伺服器出現錯誤"), 500
+
+	try:
+		category = jsonify({
+			'data': data
+		})
+		return category
+
+	finally:
+		close(c, cursor)
+		
+	
 
 app.run(port=3000)
