9de0054509fe03977062ebc2a1f8dfcab06ab196
finleypeng <weilllyox1020@gmail.com>
2022-12-06 21:51:17 +0800

    refactor: 景點相關資料庫正規化 	原本作法： 	資料庫結構爲兩塊：景點（全部除了分類）、景點的分類
    	目前問題：
    	1. 景點照片連結欄位不符合第一正規化（一個欄位只能有單一值）
    	2. 景點捷運站不符合第二正規化（消除部分相依）
    
    	調整項目：
    	1. 獨立照片連結資料表，並一個欄位只存放一筆連結
    	2. 獨立景點捷運站資料表，使用景點編號關聯並消除重複值
    	3. 調整 sql 語法

--- api/attraction/models.py
+++ api/attraction/models.py
@@ -2,23 +2,35 @@ import sys
 sys.path.append("../../")
 from modules.connect_to_db import conn, selectDb, close
 from modules.error_message import errorMessage
+from icecream import ic
 
 class Attraction:
 	#搜尋出所有（符合條件）景點
 	def fetch_all(keyword, startAtt):
 		#判斷有無關鍵字來決定 sql 語句
 		if (keyword == None):
-			sql = '''SELECT A.*, Cat.category 
-			FROM attraction as A 
-			INNER JOIN attraction_category as Cat 
-			ON A.att_id = Cat.att_id limit %s, 13'''
+			sql = '''SELECT A.*, C.category, M.mrt, I.url
+			FROM attraction as A
+			INNER JOIN attraction_category as C 
+			ON A.id = C.att_id
+			INNER JOIN attraction_mrt as M
+			ON A.id = M.att_id
+			RIGHT JOIN ( SELECT att_id, GROUP_CONCAT(url) as url 
+			FROM attraction_img GROUP BY att_id) as I
+			ON A.id = I.att_id
+			limit %s, 13'''
 			startAttFrom = (startAtt, )
 
 		if (keyword != None):
-			sql = '''SELECT A.*, C.category 
-			FROM attraction as A 
+			sql = '''SELECT A.*, C.category, M.mrt, I.url
+			FROM attraction as A
 			INNER JOIN attraction_category as C 
-			ON A.att_id = C.att_id 
+			ON A.id = C.att_id
+			INNER JOIN attraction_mrt as M
+			ON A.id = M.att_id
+			RIGHT JOIN ( SELECT att_id, GROUP_CONCAT(url) as url 
+			FROM attraction_img GROUP BY att_id) as I
+			ON A.id = I.att_id
 			where (A.name like %s or C.category = %s) 
 			limit %s, 13'''
 			startAttFrom = ('%'+keyword+'%', keyword, startAtt)
@@ -29,42 +41,47 @@ class Attraction:
 			cursor.execute(sql, startAttFrom)
 			result = cursor.fetchall()
 			attArray = []
+			imgUrlArr = []
 			start = 0
 			end = 12
 			while (start < end): 
 				if (start > len(result)-1):  #當索引值大於資料長度
 					break
-				
 				attraction = {
 					'id': result[start][0],
-					'name': result[start][2],
-					'category': result[start][10],
-					'description': result[start][8],
-					'address':result[start][3],
-					'direction': result[start][5],
-					'mrt': result[start][4],
-					'lat': float(result[start][7]),
-					'lng': float(result[start][6]),
-					'images':eval(result[start][9])
+					'name': result[start][1],
+					'category': result[start][7],
+					'description': result[start][6],
+					'address':result[start][2],
+					'direction': result[start][3],
+					'mrt': result[start][8],
+					'lat': float(result[start][5]),
+					'lng': float(result[start][4]),
+					'images':(result[start][9]).split(',')
 				}
 				attArray.append(attraction)
 				start+= 1
+			return attArray, len(result)
 		except:
-			return False
-
+			return False	
 		finally:
 			close(c, cursor)
-			return attArray, len(result)
+			
 
 	#搜尋符合 ID 的單一景點
 	def fetch_one(attractionId):
 		try:
 			c = conn()
 			cursor = selectDb(c)
-			sql = '''SELECT A.*, Cat.category 
-			FROM attraction as A 
-			INNER JOIN attraction_category as Cat 
-			ON A.att_id = Cat.att_id 
+			sql = '''SELECT A.*, C.category, M.mrt, I.url
+			FROM attraction as A
+			INNER JOIN attraction_category as C 
+			ON A.id = C.att_id
+			INNER JOIN attraction_mrt as M
+			ON A.id = M.att_id
+			RIGHT JOIN ( SELECT att_id, GROUP_CONCAT(url) as url 
+			FROM attraction_img GROUP BY att_id) as I
+			ON A.id = I.att_id
 			where A.id = %s'''
 			attId = (int(attractionId), )
 			cursor.execute(sql, attId)
@@ -77,24 +94,24 @@ class Attraction:
 			close(c, cursor)
 		
 		try:
-			data = {
+			attraction = {
 				'id': result[0],
-				'name': result[2],
-				'category': result[10],
-				'description': result[8],
-				'address':result[3],
-				'direction': result[5],
-				'mrt': result[4],
-				'lat': float(result[7]),
-				'lng': float(result[6]),
-				'images': eval(result[9])
+				'name': result[1],
+				'category': result[7],
+				'description': result[6],
+				'address':result[2],
+				'direction': result[3],
+				'mrt': result[8],
+				'lat': float(result[5]),
+				'lng': float(result[4]),
+				'images':result[9].split(',')
 			}
+			return attraction
 		except TypeError:
 			return None
 		except:
-			return False
-		finally:
-			return data
+			return False		
+
 
 	#找出所有景點分類
 	def fetch_categories():
